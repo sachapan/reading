3f05a574942ed1bbbaabc76288b48ab9d553952c
Sacha Panasuik <sachapan@gmail.com>
2023-11-27 22:14:18 -0500

    First prototype generating docx from text.

--- create_docx.py
+++ create_docx.py
@@ -1,7 +1,11 @@
 from docx import Document
 
-document = Document()
-document.add_heading('A simple text', level=1)
-document.add_paragraph('some more text ... ')
-
-document.save('docx_file.docx')
+def main():
+    f = open("text.txt", "r", encoding="utf8")
+    content = f.read()
+    document = Document()
+    # document.add_heading('A simple text', level=1)
+    document.add_paragraph(content)
+    document.save('docx_file.docx')
+if __name__ == "__main__":
+    main() 
